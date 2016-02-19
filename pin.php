bed4d9041b935561a88386389c14fb57f7d5d3a2
sumitkumardey91 <sumitkumardey91@gmail.com>
2016-02-19 14:05:12 +0530

    backend code of pin filter

---
+++ pin.php
@@ -0,0 +1,15 @@
+<?php
+mysql_connect("localhost","root","");
+mysql_select_db("prestashop1");
+if(isset($_POST["submit"]))
+{
+$take_pin=$_POST['pincode'];
+$pin_details=mysql_query("select pin from bigbasket where pin=$take_pin");
+if($pin=mysql_fetch_array($pin_details))
+ header("Location: http://localhost/prestashop1/");	
+
+else
+	header("Location: http://localhost/prestashop1/irely/");
+}
+
+?>
