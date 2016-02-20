bab882db56a694f5dee28cbe30871431bf71a9df
sumitkumardey91 <sumitkumardey91@gmail.com>
2016-02-20 17:29:34 +0530

    Update pin.php

--- pin.php
+++ pin.php
@@ -3,7 +3,7 @@ mysql_connect("localhost","root","");
 mysql_select_db("prestashop1");
 if(isset($_POST["submit"]))
 {
-$take_pin=$_POST['pincode'];
+$take_pin=$_POST['pincode'];//getting pin code from pinui.html
 $pin_details=mysql_query("select pin from bigbasket where pin=$take_pin");
 if($pin=mysql_fetch_array($pin_details))
  header("Location: http://localhost/prestashop1/");	
